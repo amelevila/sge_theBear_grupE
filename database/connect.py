2bbfa1fee6dc3a270131254df97c7ec45b246d07
Jime1805 <18ejimen@gmail.com>
2025-02-25 09:35:01 +0100

    connect.py acabat

---
+++ database/connect.py
@@ -0,0 +1,15 @@
+import psycopg2
+
+def connection_db():
+    conn = psycopg2.connect(
+        database = "the_bear",
+        password = "pass",
+        user = "user",
+        host = "localhost",
+        port = "5432"
+        )
+    return conn
+conexio = connection_db()
+print(conexio)
+conexio.close()
+print(conexio)
