b891a107e4fcba3e3c4a71fb0ee8380e3490c7fe
Jime1805 <18ejimen@gmail.com>
2025-03-18 08:55:07 +0100

    punt 3

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sge_theBear_grupE
 
+![img_1.png](img_1.png)
+![img_2.png](img_2.png)
+![img_3.png](img_3.png)
+![img_4.png](img_4.png)
 ![img.png](img.png)
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+from fileinput import close
 from typing import List
 from fastapi import FastAPI
 from services import read
+
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+from services import user
+import os
 
 app = FastAPI()
 
@@ -8,3 +15,15 @@ app = FastAPI()
 async def read_root():
     result = read.registre()
     return result
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SQLModel.metadata.create_all(engine)
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
