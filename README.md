dea6e1d66eb9e5b8e49b27cd2112279721f82649
1zan06 <izan.hidalgo20062gmail.com>
2025-03-18 08:57:11 +0100

    Activitat 2 acabada

--- README.md
+++ README.md
@@ -7,3 +7,7 @@
 ![img_5.png](img_5.png)
 ![img_6.png](img_6.png)
 ![img_7.png](img_7.png)
+RESULTAT ACTIVITAT FASTAPI + BD:
+![img_8.png](img_8.png)
+ESTRUCTURA FINAL:
+![img_9.png](img_9.png)
--- main.py
+++ main.py
@@ -8,3 +8,52 @@ app = FastAPI()
 async def read_root():
     result = read.registre()
     return result
+
+from fastapi import Depends
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+from services import user
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+SQLModel.metadata.create_all(engine)
+
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/users/", response_model= list[dict])
+def read_user(db:Session = Depends(get_db)):
+    result = user.get_all_users(db)
+    return result
+
+
+from schema.users_sch import users_schema
+from sqlmodel import Session, select
+from models.User import User
+
+def get_all_users(db:Session):
+    sql_read = select(User)
+    users = db.exec(sql_read).all()
+    return users_schema(users)
+
+@app.post("/users/", response_model=dict)
+def create_user(name: str, email:str, db:Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
+    return result
+
+def add_new_user(name: str, email:str, db:Session):
+    db_user = User(name=name, email=email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return {"Created user succesfully"}
