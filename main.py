4841a30e0456074c478ff3371637611049d37a98
amelevila <amelevila78@gmail.com>
2025-03-18 08:48:46 +0100

    Completada activitat FASTAPI + BD

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 from typing import List
-from fastapi import FastAPI
-from services import read
+from fastapi import FastAPI, Depends
+from sqlmodel import SQLModel, create_engine, Session
+from dotenv import load_dotenv
+from services import read, user
+import os
 
 app = FastAPI()
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+SQLModel.metadata.create_all(engine)
+
+def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/root", response_model=List[dict])
 async def read_root():
     result = read.registre()
+    return result
+
+@app.get("/users/", response_model= list[dict])
+def read_user(db:Session = Depends(get_db)):
+    result = user.get_all_users(db)
+    return result
+
+@app.post("/users/", response_model=dict)
+def create_user(name: str, email:str, db:Session = Depends(get_db)):
+    result = user.add_new_user(name, email, db)
     return result
