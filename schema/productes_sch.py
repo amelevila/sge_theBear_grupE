ef8dbae01d12a2ef68a06f77c041472d9144e8a8
amelevila <amelevila78@gmail.com>
2025-04-29 08:39:29 +0200

    producte_schema a singular

--- schema/productes_sch.py
+++ schema/productes_sch.py
@@ -3,5 +3,5 @@ def producte_schema(producte) -> dict:
     return response
 
 def productes_schema(productes)-> list[dict]:
-    response = [productes_schema(producte) for producte in productes]
+    response = [producte_schema(producte) for producte in productes]
     return response
